4451405a98b91adbcb7c969fe65ea5b652c6f1eb
alexwine36 <alexwine36@gmail.com>
2017-03-21 20:17:18 -0600

    Version 0.1.26_7

--- package.js
+++ package.js
@@ -2,12 +2,13 @@ Package.describe({
 	name: "alexwine:pdfmake",
 	summary: "Let's you generate PDF's on the client and provides handy features for Layout & Styling.",
 	author: "Alex Wine <alexwine@awts.io>",
-	version: "0.1.26",
+	version: "0.1.26_7",
 	documentation: 'readme.md',
 	git: "https://github.com/alexwine36/pdfmake.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('METEOR@1.0');
+	api.addAssets('build/pdfmake.min.js.map', 'client');
   api.addFiles(['build/pdfmake.min.js', 'build/vfs_fonts.js'], 'client');
 });
